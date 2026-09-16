38bbe362703ea01e837fefc29353dab8fb5b4970
Dmitriy Potykalov <dmitriy.potykalov@gmail.com>
2026-09-16 07:36:46 +0300

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+import GamePlay from "./Game/GamePlay";
+import GameController from "./Game/GameController";
+import GameStateService from "./Game/GameStateService";
+
+const gamePlay = new GamePlay();
+gamePlay.bindToDOM(document.querySelector("#game-container"));
+
+const stateService = new GameStateService(localStorage);
+
+const gameCtrl = new GameController(gamePlay, stateService);
+gameCtrl.init();

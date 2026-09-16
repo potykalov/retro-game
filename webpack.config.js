0d1d46c5a62e0241c7e4212fd639cad68637d46e
Dmitriy Potykalov <dmitriy.potykalov@gmail.com>
2026-09-16 07:10:24 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const config = {
+  entry: "./src/index.js",
+
+  devServer: {
+    port: 8080,
+    open: true,
+    hot: true,
+    watchFiles: ["src/**/*.html"],
+  },
+
+  module: {
+    rules: [
+       {
+        test: /\.m?js$/,
+        resolve: {
+        fullySpecified: false,
+       },
+       },
+       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.html$/i,
+        use: "html-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif|webp|avif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [".js"],
+  },
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
+};
+
+export default config;
